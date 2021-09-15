1bdd5d9e32ae3e9b269221cf40842e63252e1869
mrdoi82 <78681523+mrdoi82@users.noreply.github.com>
2021-09-15 18:22:24 +0430

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -560,10 +560,10 @@ async def inline_handler(event):  # sourcery no-metrics
     else:
         buttons = [
             (
-                Button.url("Source code", "https://github.com/Jisan09/catuserbot"),
+                Button.url("i am", "https://t.me/Binam012345"),
                 Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
+                    "me",
+                    "https://t.me/Binam012345",
                 ),
             )
         ]
@@ -572,14 +572,14 @@ async def inline_handler(event):  # sourcery no-metrics
             url=CATLOGO, size=0, mime_type="image/jpeg", attributes=[]
         )
         text, msg_entities = await event.client._parse_message_text(
-            "𝗗𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗼𝘄𝗻 𝗖𝗮𝘁𝗨𝘀𝗲𝗿𝗯𝗼𝘁.", "md"
+            "I AM SEYYED", "md"
         )
         result = types.InputBotInlineResult(
             id=str(uuid4()),
             type="photo",
-            title="𝘾𝙖𝙩𝙐𝙨𝙚𝙧𝙗𝙤𝙩",
-            description="Deploy yourself",
-            url="https://github.com/Jisan09/catuserbot",
+            title="SEYYED",
+            description="I AM HERE",
+            url="https://t.me/Binam012345",
             thumb=photo,
             content=photo,
             send_message=types.InputBotInlineMessageMediaAuto(
